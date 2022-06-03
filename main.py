baf470c7cfdc5c541f705a795bae601052bf2b38
Methanon Keaokrachang <48640302+MarkystarkDotCO@users.noreply.github.com>
2022-06-03 15:53:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,26 +6,28 @@ from vmware.mjolnir.infrastructure import ssh_server
 from vmware.mjolnir.library.api import restapi 
 from vmware.mjolnir.remote_machine_object import RemoteMachineObject
 
-
+'''
 ft = fault_teardown.FaultTeardown
 print(ft.wait_for_remediation_to_complete)
 print(ft.remediate_all_faults)
+'''
 
-
-
+'''
 example= [{
                         'name': '',
                         'ip': '',
                         'username': '',
                         'password': '',
                         'ssh_port': ''}]
+'''
 
-remote = RemoteMachineObject(example)
 
-fi = fault_injection.FaultInjection( remote, 'fault_type', 'fault_sub_type', 'fault_payload')
+#remote = RemoteMachineObject(example)
 
-print(fi.add_remote_endpoint)
-print(fi.inject_fault)
+#fi = fault_injection.FaultInjection( remote, 'fault_type', 'fault_sub_type', 'fault_payload')
+
+#print(fi.add_remote_endpoint)
+#print(fi.inject_fault)
 
 
 #print('running OK')
