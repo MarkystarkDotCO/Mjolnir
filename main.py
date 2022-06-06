eae98171444a70130f5a6ee73cf48b6bf812c970
Methanon Keaokrachang <48640302+MarkystarkDotCO@users.noreply.github.com>
2022-06-06 20:19:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 from vmware.mjolnir import fault_injection
-from vmware.mjolnir import fault_teardown
 from vmware.mjolnir.remote_machine_object import RemoteMachineObject
-from vmware.mjolnir.constants import Constants
+
+name = "107"
+ip = '192.168.101.107'
+username = "cmkldev"
+password = "#2022%esx@CMKL"
+ssh_port = 22
+
+fault_type = "INFRA"
+fault_sub_type = "CPU"
+fault_payload="cpuLoad"
 
 
-const = Constants()
-print("MANGLE_SERVER   :",const.MANGLE_SERVER)
-print("MANGLE_USERNAME :",const.MANGLE_USERNAME)
-print("MANGLE_PASSWORD :",const.MANGLE_PASSWORD)
+remote = [RemoteMachineObject(name = name, ip=ip, username= username, password = password, ssh_port = ssh_port)
+]
 
-rem = RemoteMachineObject
+fi = fault_injection.FaultInjection( remote, fault_type, fault_sub_type, fault_payload)
 
-ft = fault_teardown.FaultTeardown()
-#print(ft.wait_for_remediation_to_complete)
-print(ft.remediate_all_faults)
 
-fi = fault_injection.FaultInjection('','','')
-print(fi.get_fault_type)
+
+
 
 
 '''
-example= [{
-                        'name': '',
-                        'ip': '',
-                        'username': '',
-                        'password': '',
-                        'ssh_port': ''}]
+Bug found
+
+1.ConnectionRefusedError: [Errno 111] Connection refused
+
+2.Failed to establish a new connection: [Errno 111] Connection refused'))
+
+3.requests.exceptions.ConnectionError: HTTPSConnectionPool(host='192.168.101.180', port=443): Max retries exceeded 
+with url: /mangle-services/rest/api/v1/endpoints/credentials 
+(Caused by NewConnectionError('<urllib3.connection.HTTPSConnection object at 0x7fd2263728c0>: 
+Failed to establish a new connection: [Errno 111] Connection refused'))
+
+how to solve : 
+method 1
+https://stackoverflow.com/questions/23013220/max-retries-exceeded-with-url-in-requests
+
+
 '''
-#remote = RemoteMachineObject(example)
-
-#fi = fault_injection.FaultInjection( remote, 'fault_type', 'fault_sub_type', 'fault_payload')
-
-#print(fi.add_remote_endpoint)
-#print(fi.inject_fault)
-
-
-#print('running OK')
